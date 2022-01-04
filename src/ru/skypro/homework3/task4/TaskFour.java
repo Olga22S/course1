0aca8abfe47199c861ccae267303451e5c7ab976
Olga22S <22031992Les>
2022-01-04 18:26:26 +0300

    Homework 3 high difficult is done

---
+++ src/ru/skypro/homework3/task4/TaskFour.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework3.task4;
+
+public class TaskFour {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ": ping pong");
+            } else if (i % 3 == 0) {
+                System.out.println(i + ": ping");
+            } else if (i % 5 == 0) {
+                System.out.println(i + ": pong");
+            } else {
+                System.out.println(i + ":");
+            }
+        }
+    }
+}
