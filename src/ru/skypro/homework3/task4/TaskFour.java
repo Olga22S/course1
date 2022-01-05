39b97f31d07826140db4b406c60f94dc30083b37
Olga22S <22031992Les>
2022-01-05 22:18:31 +0300

    task 4 was remade

--- src/ru/skypro/homework3/task4/TaskFour.java
+++ src/ru/skypro/homework3/task4/TaskFour.java
@@ -5,15 +5,14 @@ public class TaskFour {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 30; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i + ": ping pong");
-            } else if (i % 3 == 0) {
-                System.out.println(i + ": ping");
-            } else if (i % 5 == 0) {
-                System.out.println(i + ": pong");
-            } else {
-                System.out.println(i + ":");
+            System.out.print(i + ":");
+            if (i % 3 == 0) {
+                System.out.print(" ping");
             }
+            if (i % 5 == 0) {
+                System.out.print(" pong");
+            }
+            System.out.println();
         }
     }
 }
